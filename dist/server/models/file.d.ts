abbf57d66b2f3c0f4118469a42691ba90f0716d7
Vitaliy Semenchenko <v.semenchenko@nodeart.io>
2024-06-21 10:44:10 +0300

    chore: provide dist/ to allow install from github

---
+++ dist/server/models/file.d.ts
@@ -0,0 +1,18 @@
+/// <reference types="node" />
+import { ReadStream } from "fs";
+export interface File {
+    name: string;
+    hash: string;
+    mime: string;
+    path?: string;
+    ext: string;
+    folder?: string;
+    folderPath?: string;
+    width?: number;
+    height?: number;
+    size?: number;
+    getStream: () => ReadStream;
+}
+export interface SourceFile extends File {
+    tmpWorkingDirectory: string;
+}
